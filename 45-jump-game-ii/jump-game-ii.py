459be98493da33b791a6ccbd03da0f58989f8990
siyabisenvt45 <bisensiya5@gmail.com>
2025-05-18 20:09:20 +0530

    Time: 5 ms (86.81%) | Memory: 13.1 MB (69.88%) - LeetSync

---
+++ 45-jump-game-ii/jump-game-ii.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def jump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = len(nums)
+        if n == 1:
+            return 0
+
+        jumps = 0
+        current_end = 0
+        farthest = 0
+
+        for i in range(n - 1):
+            farthest = max(farthest, i + nums[i])
+            if i == current_end:
+                jumps += 1
+                current_end = farthest
+                if current_end >= n - 1:
+                    break
+
+        return jumps
